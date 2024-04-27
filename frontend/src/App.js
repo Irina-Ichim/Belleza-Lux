63b81a9a61d6465cfeca6242fa2c73a393a53085
Irina Ichim <onlyirina7@gmail.com>
2024-04-27 02:50:52 +0200

    comprobando ruta

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Importa BrowserRouter y Routes
 import './App.css';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
@@ -8,9 +8,8 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar />  {/* Render the navbar outside of Routes */}
+        <Navbar />  {/* Renderiza la barra de navegación fuera de Routes */}
         <Routes>
-          
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
